90a7846719b233daec1f291f572c8bb2d2feb8af
bproencampos <bruno.dataengineer@outlook.com>
2023-09-12 23:34:18 -0300

    dependecias para o lambda
    dependecias para o lambda

--- scritps/lambda_extract_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-url_csv = "https://raw.githubusercontent.com/bproencampos/integration-flow-challenge/develop/data_source/movies_metadatas.csv"
-
-data = pd.read_csv(url_csv)
-
-# print(df.head(5))
