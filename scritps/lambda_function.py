534f5a24aa247418a016e7b9b3217e520adfe136
bproencampos <bruno.dataengineer@outlook.com>
2023-09-13 00:11:47 -0300

    update
    update

--- scritps/lambda_function.py
+++ scritps/lambda_function.py
@@ -7,14 +7,14 @@ def lambda_handler(event, context):
     # Informacoes do arquivo no Github
     github_user = 'bproencampos'
     github_repo = 'integration-flow-challenge'
-    github_file_path = 'bproencampos/integration-flow-challenge/movies_metadatas.csv'
+    github_file_path = 'data_source/movies_metadatas.csv'
     
     # URL do arquivo
     github_url = f'https://raw.githubusercontent.com/bproencampos/integration-flow-challenge/develop/data_source/movies_metadatas.csv'
     
     # Nome do arquivo no S3
-    bucket_name = 'integrationflowbucket'
-    filename = '/tmp/movies_metadatas.csv'    # Precisa ser TMP????
+    s3_bucket = 'integrationflowbucket'
+    s3_key = '/tmp/movies_metadatas.csv'    # Precisa ser TMP????
     
     # Cria cliente s3
     s3 = boto3.client('s3')
@@ -24,7 +24,7 @@ def lambda_handler(event, context):
         response = requests.get(github_url)
         if response.status_code == 200:
             # Carrega conteudo do arquivo no s3
-            s3.put_object(Bucket=bucket_name,key=filename,Body=response.text)
+            s3.put_object(Bucket=s3_bucket,Key=s3_key,Body=response.text)
             return{
                 'statusCode': 200,
                 'body': 'Arquivo CSV carregado com sucesso no S3.'
